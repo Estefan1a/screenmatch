c7bb97e1f4678bbd3c32eab41d8084f7f196efdf
Estefanía González <estefania.gonzre@gmail.com>
2025-07-31 18:14:44 -0600

    Actualiza Serie.java

--- src/com/aluracursos/screenmatch/modelos/Serie.java
+++ src/com/aluracursos/screenmatch/modelos/Serie.java
@@ -5,6 +5,10 @@ public class Serie extends Titulo {
     private int episodiosPorTemporadas;
     private int minutosPorEpisodio;
 
+    public Serie(String nombre, int fechaDeLanzamiento) {
+        super(nombre, fechaDeLanzamiento);
+    }
+
     @Override
     public int getDuracionEnMinutos(){
         return temporadas * episodiosPorTemporadas * minutosPorEpisodio;
@@ -34,4 +38,10 @@ public class Serie extends Titulo {
     public void setMinutosPorEpisodio(int minutosPorEpisodio) {
         this.minutosPorEpisodio = minutosPorEpisodio;
     }
+
+    @Override
+    public String toString() {
+        return "Serie: " + this.getNombre()+ " ("+ this.getFechaDeLanzamiento() + ")";
+    }
+
 }
