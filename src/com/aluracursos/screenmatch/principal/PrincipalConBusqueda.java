8c92a99f2d49069e803e6abe9084693b5ac7ae61
Estefanía González <estefania.gonzre@gmail.com>
2025-08-05 03:00:29 -0600

    Crea PrincipalConBusqueda.java

---
+++ src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -0,0 +1,37 @@
+package com.aluracursos.screenmatch.principal;
+
+import com.aluracursos.screenmatch.modelos.Titulo;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalConBusqueda {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Escriba el nombre de una película");
+        var busqueda = teclado.nextLine();
+
+        String direccion = "https://www.omdbapi.com/?t=" + busqueda + "&apikey=3e33ea23";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(direccion))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson= new Gson();
+        Titulo miTitulo = gson.fromJson(json, Titulo.class);
+        System.out.println("Titulo: " + miTitulo.getNombre());
+
+    }
+}
